eda09e3be06f7ce9948863f5e9b24715b0e22637
Daniel Bound <boun.d@hotmail.com>
2024-11-30 13:39:23 +1100

    [feature/basic-game] Updating next.config for styled-components

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,13 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  compiler: {
+    styledComponents: true
+  },
+  webpack: (config) => {
+    config.cache = false
+    return config
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
